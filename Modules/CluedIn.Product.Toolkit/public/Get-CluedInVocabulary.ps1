a8a6c560a36ffe45d7c16dd1d12f25c56a788420
jrcluedin <jjr@cluedin.com>
2023-10-18 16:51:38 +0100

    Filter results out once everything has been received

--- Modules/CluedIn.Product.Toolkit/public/Get-CluedInVocabulary.ps1
+++ Modules/CluedIn.Product.Toolkit/public/Get-CluedInVocabulary.ps1
@@ -39,5 +39,12 @@ function Get-CluedInVocabulary {
         if ($result.data.management.vocabularies.data.count -ge $total) { break }
     }
 
+    # This is cleanup from client side. Our GraphQL may not support filtering at runtime
+    # Something to look into?
+    $result.data.management.vocabularies.data = $result.data.management.vocabularies.data | 
+        Where-Object {$_.isCluedInCore -eq $false}
+    $result.data.management.vocabularies.total = $result.data.management.vocabularies.data.count
+    #
+    
     return $result
 }

710f49d52f109dc29a043a15e607e706cb6c379f
jrcluedin <jjr@cluedin.com>
2023-12-15 11:30:46 +0000

    Update help

--- Modules/CluedIn.Product.Toolkit/public/Set-CluedInDataSetMapping.ps1
+++ Modules/CluedIn.Product.Toolkit/public/Set-CluedInDataSetMapping.ps1
@@ -1,13 +1,13 @@
 function Set-CluedInDataSetMapping {
     <#
         .SYNOPSIS
-        GraphQL Query: Sets an annotation mapping to a dataset
+        GraphQL Query: Sets an annotation mapping on a dataset
 
         .DESCRIPTION
-        GraphQL Query: Set an annotation mapping to a dataset
+        GraphQL Query: Set an annotation mapping on a dataset
 
         .EXAMPLE
-        PS> New-CluedInDataSetMapping -DataSetId 'cf5a8ac4-95eb-42e5-a93e-a372e22aa439' -Object $annotationObject
+        PS> Set-CluedInDataSetMapping -DataSetId 'cf5a8ac4-95eb-42e5-a93e-a372e22aa439' -FieldMappings @(@{ originalField = 'key'; key = 'vocabkey'; id = 'guid-of-mapping-id' })
     #>
 
     [CmdletBinding()]

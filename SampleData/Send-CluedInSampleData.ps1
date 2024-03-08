d54a2bc52e927c0decd756806f7c6dcdbd14450a
jrcluedin <jjr@cluedin.com>
2024-03-08 15:42:15 +0000

    chore: Clean up sample data

--- SampleData/Send-CluedInSampleData.ps1
+++ SampleData/Send-CluedInSampleData.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
-    [string]$BaseURL = '51.140.229.75.sslip.io',
-    [string]$Organization = 'cluedin'
+    [parameter(Mandatory)][string]$BaseURL,
+    [parameter(Mandatory)][string]$Organization
 )
 
 Connect-CluedInOrganisation -BaseURL $BaseURL -Organisation $Organization
@@ -9,23 +9,33 @@ Connect-CluedInOrganisation -BaseURL $BaseURL -Organisation $Organization
 if ([version]${env:CLUEDIN_CURRENTVERSION} -lt [version]'4.0.0') { throw "Unable to support sample set data" }
 
 Write-Host "Creating data source set"
-$dataSourceSetObject = Get-Content -Path './SampleData/jsons/Data/SourceSets/DataSourceSet.json' | ConvertFrom-Json -Depth 99
+$currentDirectory = $PSScriptRoot
+$jsonPath = Join-Path -Path $currentDirectory -ChildPath 'sample-person.json'
+
+$dataSourceSetPath = Join-Path -Path $currentDirectory -ChildPath 'jsons/Data/SourceSets/DataSourceSet.json'
+$dataSourceSetObject = Get-Content -Path $dataSourceSetPath | ConvertFrom-Json -Depth 99
 $dataSourceSet = New-CluedInDataSourceSet -Object $dataSourceSetObject.data.inbound.dataSourceSets.data
 
 Write-Host "Creating data source"
-$dataSourceObject = Get-Content -Path './SampleData/jsons/Data/Sources/1-DataSource.json' | ConvertFrom-Json -Depth 99
+$dataSourcePath = Join-Path -Path $currentDirectory -ChildPath 'jsons/Data/Sources/1-DataSource.json'
+$dataSourceObject = Get-Content -Path $dataSourcePath | ConvertFrom-Json -Depth 99
 $dataSourceObject.data.inbound.dataSource.dataSourceSet.id = $dataSourceSet.data.inbound.createDataSourceSet
 $dataSource = New-CluedInDataSource -Object $dataSourceObject.data.inbound.dataSource
 
 Write-Host "Creating data set"
-$dataSetObject = Get-Content -Path './SampleData/jsons/Data/Sets/925268EC-8908-44E2-8D0C-D83FE50937D0-DataSet.json' | ConvertFrom-Json -Depth 99
-$dataSetObject.data.inbound.dataSet.dataSourceId = $dataSource.data.inbound.createDataSource.id
-$dataSetObject.data.inbound.dataSet.dataSource.id = $dataSource.data.inbound.createDataSource.id
-$dataSet = New-CluedInDataSet -Object $dataSetObject.data.inbound.dataSet
+$dataSetPath = Join-Path -Path $currentDirectory -ChildPath 'jsons/Data/Sets/'
+$dataSets = Get-ChildItem -Path $dataSetPath
+foreach ($set in $dataSets) {
+    $dataSetObject = Get-Content -Path $set.FullName | ConvertFrom-Json -Depth 99
+    $dataSetObject.data.inbound.dataSet.dataSourceId = $dataSource.data.inbound.createDataSource.id
+    $dataSetObject.data.inbound.dataSet.dataSource.id = $dataSource.data.inbound.createDataSource.id
 
-$endpointId = $dataSet.data.inbound.createDataSets.id
+    $dataSet = New-CluedInDataSet -Object $dataSetObject.data.inbound.dataSet
 
-Write-host "Sending data to data set id '$($endpointId)'"
-$jsonBlob = Get-Content -Path './SampleData/sample-people.json' -Raw
-$sendResult = Send-CluedInIngestionData -Json $jsonBlob -IngestionEndpoint $endpointId
-if ($sendResult.error -eq 'True' ) { throw "Uh oh, we have a throw" }
+    $endpointId = $dataSet.data.inbound.createDataSets.id
+
+    Write-host "Sending data to data set id '$($endpointId)'"
+    $jsonBlob = Get-Content -Path $jsonPath -Raw
+    $sendResult = Send-CluedInIngestionData -Json $jsonBlob -IngestionEndpoint $endpointId
+    if ($sendResult.error -eq 'True' ) { throw "Uh oh, we have a throw" }
+}

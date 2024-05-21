0bc115afef67ec4f94cce02dbe962dfd4a7c68ca
jrcluedin <jjr@cluedin.com>
2024-05-21 10:02:08 +0100

    chore: Tidy up Set-CluedInDataSetMapping

--- Modules/CluedIn.Product.Toolkit/public/Set-CluedInDataSetMapping.ps1
+++ Modules/CluedIn.Product.Toolkit/public/Set-CluedInDataSetMapping.ps1
@@ -7,25 +7,14 @@ function Set-CluedInDataSetMapping {
         GraphQL Query: Set an annotation mapping on a dataset
 
         .EXAMPLE
-        PS> Set-CluedInDataSetMapping -DataSetId 'cf5a8ac4-95eb-42e5-a93e-a372e22aa439' -FieldMappings @(@{ originalField = 'key'; key = 'vocabkey'; id = 'guid-of-mapping-id' })
+        PS> Set-CluedInDataSetMapping -DataSetId 'cf5a8ac4-95eb-42e5-a93e-a372e22aa439' -PropertyMappingConfiguration $propertyMappingConfig
     #>
 
     [CmdletBinding()]
     param(
         [guid]$DataSetId,
-        # [array]$FieldMappings
         [hashtable]$PropertyMappingConfiguration
     )
-
-    # $queryContent = Get-CluedInGQLQuery -OperationName 'updateAnnotationMappingInDataSet'
-
-    # $query = @{
-    #     variables = @{
-    #         dataSetId = $DataSetId
-    #         fieldMappings = $FieldMappings
-    #     }
-    #     query = $queryContent
-    # }
 
     $queryContent = Get-CluedInGQLQuery -OperationName 'updatePropertyMappingInClueMappingConfig'
 

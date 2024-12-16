a52c29774f2ecfd00793887de090c14192097779
Arvindran Kalidas <aka@cluedin.com>
2024-12-16 17:45:26 +0800

    fix: Update logic to create new export target to match required fields in v4.4.0

--- Scripts/Import-CluedInConfig.ps1
+++ Scripts/Import-CluedInConfig.ps1
@@ -722,13 +722,16 @@ foreach ($target in $exportTargets) {
                 $hasTarget = $true
                 $id = $exportTarget.id
                 break
-            }elseif(($exportTarget.accountDisplay -eq $targetObject.accountDisplay) -and ($exportTarget.providerId -eq $targetObject.providerId))
+            }
+            elseif(($exportTarget.accountDisplay -eq $targetObject.accountDisplay) -and ($exportTarget.providerId -eq $targetObject.providerId))
             {
                 Write-Verbose "Found match on display name :: $($exportTarget.accountDisplay) == $($targetObject.accountDisplay)"
                 $hasTarget = $true
                 $id = $exportTarget.id
                 break
-            } elseif(($exportTargetDisplayName -eq $targetDisplayName) -and ($exportTarget.providerId -eq $targetObject.providerId)) {
+            }
+            elseif(($exportTargetDisplayName -eq $targetDisplayName) -and ($exportTarget.providerId -eq $targetObject.providerId))
+            {
                 Write-Verbose "Found match on assumed display name :: $($exportTarget.accountDisplay) == $($targetObject.helperConfiguration.accountName) $($targetObject.helperConfiguration.fileSystemName) $($targetObject.helperConfiguration.directoryName)"
                 $hasTarget = $true
                 $id = $exportTarget.id
@@ -745,7 +748,17 @@ foreach ($target in $exportTargets) {
         }
         
         Write-Verbose "Creating Export Target $($targetObject.helperConfiguration)"
-        $targetResult = New-CluedInExportTarget -ConnectorId $targetObject.providerId -Configuration $targetObject.helperConfiguration
+        if ([version]${env:CLUEDIN_CURRENTVERSION} -ge [version]"4.4.0")
+        {
+            # If the accountDisplay is null, we use id instead as the account display
+            $accountDisplay = if ($targetObject.accountDisplay) { $targetObject.accountDisplay } else { $targetObject.id }
+            $targetResult = New-CluedInExportTarget -ConnectorId $targetObject.providerId -Configuration $targetObject.helperConfiguration -AccountDisplay $accountDisplay
+        }
+        else
+        {
+            $targetResult = New-CluedInExportTarget -ConnectorId $targetObject.providerId -Configuration $targetObject.helperConfiguration
+        }
+
         $id = $targetResult.data.inbound.createConnection.id
         if (!$id) { Write-Warning "Unable to get Id of target. Importing on top of existing export targets can be flakey. Please manually investigate."; continue }
     }

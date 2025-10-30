9ef722f8ce638e90cf1addafff9eac2eb941772b
Nick Barlow <nicholasbarlow1@gmail.com>
2025-10-30 10:48:43 +0000

    feat: Remove passwords so they are notstore in the JSON files (#90)

--- Modules/CluedIn.Product.Toolkit/public/Import/Import-ExportTargets.ps1
+++ Modules/CluedIn.Product.Toolkit/public/Import/Import-ExportTargets.ps1
@@ -1,4 +1,4 @@
-function Import-ExportTargets{
+function Import-ExportTargets {
     <#
         .SYNOPSIS
         Imports deduplication projects
@@ -35,9 +35,6 @@ function Import-ExportTargets{
     $lookupConnectors = @()
 
     $currentExportTargets = (Get-CluedInExportTargets).data.inbound.connectorConfigurations.configurations
-    $targetExists = $targetObject.accountId -in $currentExportTargets.accountId
-
-    $allUsers = (Get-CluedInUsers).data.administration.users # Caching for use down below
 
     foreach ($target in $exportTargets) {
         $targetJson = Get-Content -Path $target.FullName | ConvertFrom-Json -Depth 20
@@ -51,40 +48,46 @@ function Import-ExportTargets{
         }
 
         $cleanProperties.ForEach({
-            if ($_ -in $targetProperties) { $targetObject.helperConfiguration.$_ = $null }
-        })
+                if ($_ -in $targetProperties) { $targetObject.helperConfiguration.$_ = $null }
+            })
         
         $hasTarget = $false
-        $id = $null;
+        $id = $null
+        $matchedExportTarget = $null
 
-        if($null -ne $currentExportTargets)
-        {
-            foreach($exportTarget in $currentExportTargets) {
+        if ($null -ne $currentExportTargets) {
+            foreach ($exportTarget in $currentExportTargets) {
                 $exportTargetDisplayName = $exportTarget.accountDisplay.Trim()
                 $targetDisplayName = "$($targetObject.helperConfiguration.accountName) $($targetObject.helperConfiguration.fileSystemName) $($targetObject.helperConfiguration.directoryName)"
 
-                if(($targetObject.accountId -eq $currentExportTargets.accountId) -and ($null -ne $targetObject.accountId) -and ('' -ne $targetObject.accountId) -and ('0' -ne $targetObject.accountId))
-                {
+                if (($targetObject.accountId -eq $currentExportTargets.accountId) -and ($null -ne $targetObject.accountId) -and ('' -ne $targetObject.accountId) -and ('0' -ne $targetObject.accountId)) {
                     Write-Verbose "Found match on account id :: $($exportTarget.accountDisplay) == $($targetObject.accountDisplay)"
                     $hasTarget = $true
                     $id = $exportTarget.id
+                    $matchedExportTarget = $exportTarget
                     break
                 }
-                elseif(($exportTarget.accountDisplay -eq $targetObject.accountDisplay) -and ($exportTarget.providerId -eq $targetObject.providerId))
-                {
+                elseif (($exportTarget.accountDisplay -eq $targetObject.accountDisplay) -and ($exportTarget.providerId -eq $targetObject.providerId)) {
                     Write-Verbose "Found match on display name :: $($exportTarget.accountDisplay) == $($targetObject.accountDisplay)"
                     $hasTarget = $true
                     $id = $exportTarget.id
+                    $matchedExportTarget = $exportTarget
                     break
                 }
-                elseif(($exportTargetDisplayName -eq $targetDisplayName) -and ($exportTarget.providerId -eq $targetObject.providerId))
-                {
+                elseif (($exportTargetDisplayName -eq $targetDisplayName) -and ($exportTarget.providerId -eq $targetObject.providerId)) {
                     Write-Verbose "Found match on assumed display name :: $($exportTarget.accountDisplay) == $($targetObject.helperConfiguration.accountName) $($targetObject.helperConfiguration.fileSystemName) $($targetObject.helperConfiguration.directoryName)"
                     $hasTarget = $true
                     $id = $exportTarget.id
+                    $matchedExportTarget = $exportTarget
                     break
                 }
             }
+        }
+     
+        # If we found an existing export target, restore password fields into the outgoing configuration
+        if ($hasTarget -and $matchedExportTarget) {
+            $matchedExportTargetConfig = (Get-CluedInExportTarget -Id $matchedExportTarget.id).data.inbound.connectorConfiguration
+            Restore-HelperPasswords -TargetObject $targetObject -MatchedExportTarget $matchedExportTargetConfig
         }
 
         if ($hasTarget -eq $false) {
@@ -117,4 +120,115 @@ function Import-ExportTargets{
     }
 
     return $lookupConnectors
+}
+
+
+# Discover names of password fields from connector authMethods (dynamic)
+function Get-PasswordPropertyNames {
+    param(
+        [Parameter(Mandatory)]
+        [psobject] $AuthMethods
+    )
+
+    $names = New-Object System.Collections.Generic.HashSet[string] ([System.StringComparer]::OrdinalIgnoreCase)
+    if ($null -eq $AuthMethods) { return $names }
+
+    foreach ($methodProp in $AuthMethods.PSObject.Properties) {
+        $val = $methodProp.Value
+        if ($null -eq $val) { continue }
+
+        if ($val -is [System.Collections.IEnumerable] -and -not ($val -is [string])) {
+            foreach ($item in $val) {
+                if ($item -and ($item.type -eq 'password') -and $item.name) {
+                    $null = $names.Add([string]$item.name)
+                }
+            }
+        }
+        elseif ($val -is [psobject]) {
+            if ($val.type -eq 'password' -and $val.name) {
+                $null = $names.Add([string]$val.name)
+            }
+        }
+    }
+
+    return $names
+}
+
+# Try to locate authMethods on either targetObject, exportTarget, or by calling a connector fetcher
+function Get-ConnectorAuthMethods {
+    param(
+        [psobject] $ExportTarget
+    )
+
+    return $ExportTarget.connector?.authMethods
+}
+
+# Merge password values from an existing export target into the targetObject's helperConfiguration
+function Restore-HelperPasswords {
+    param(
+        [Parameter(Mandatory)] [psobject] $TargetObject,
+        [Parameter(Mandatory)] [psobject] $MatchedExportTarget
+    )
+
+    # Find authMethods -> figure out which fields are 'password'
+    $authMethods = Get-ConnectorAuthMethods -ExportTarget $MatchedExportTarget
+    if ($null -eq $authMethods) { 
+        return 
+    }
+
+    $passwordNames = Get-PasswordPropertyNames -AuthMethods $authMethods
+    if ($passwordNames.Count -eq 0) { 
+        return 
+    }
+
+    # Where we copy *to*
+    $destHelper = $TargetObject.helperConfiguration
+    if ($null -eq $destHelper) {
+        # create if absent so downstream cmdlets have a full config
+        $TargetObject | Add-Member -NotePropertyName helperConfiguration -NotePropertyValue (@{}) -Force
+        $destHelper = $TargetObject.helperConfiguration
+    }
+
+    # Some objects use a nested 'helperConfiguration' key inside helperConfiguration (seen in a few exports)
+    $nestedDest = $destHelper.helperConfiguration
+    if ($null -ne $nestedDest -and -not ($nestedDest -is [hashtable] -or $nestedDest -is [psobject])) {
+        $nestedDest = $null # ignore weird types
+    }
+
+    # Where we copy *from*
+    $sourceHelper = $MatchedExportTarget.helperConfiguration ?? $MatchedExportTarget.connectorConfiguration?.helperConfiguration
+    if ($null -eq $sourceHelper) {
+        return 
+    }
+
+    foreach ($name in $passwordNames) {
+        $srcProp = $sourceHelper.PSObject.Properties[$name]
+        if ($srcProp -and $null -ne $srcProp.Value -and "$($srcProp.Value)".Length -gt 0) {
+
+            # Only backfill if missing or empty on destination (avoid clobbering explicit values)
+            $destHas = $destHelper.PSObject.Properties[$name]
+            $needsWrite = $true
+            if ($destHas) {
+                $needsWrite = ($null -eq $destHas.Value -or "$($destHas.Value)".Length -eq 0)
+            }
+
+            if ($needsWrite) {
+                $destHelper | Add-Member -NotePropertyName $name -NotePropertyValue $srcProp.Value -Force
+            }
+
+            # Also set nested helperConfiguration if it exists and is intended to carry the same keys
+            if ($nestedDest) {
+                $ndHas = $nestedDest.PSObject.Properties[$name]
+                $ndNeedsWrite = $true
+                if ($ndHas) {
+                    $ndNeedsWrite = ($null -eq $ndHas.Value -or "$($ndHas.Value)".Length -eq 0)
+                }
+                if ($ndNeedsWrite) {
+                    $nestedDest | Add-Member -NotePropertyName $name -NotePropertyValue $srcProp.Value -Force
+                    # ensure the nested object is written back
+                    $destHelper.helperConfiguration = $nestedDest
+                }
+            }
+        }
+    }
 }

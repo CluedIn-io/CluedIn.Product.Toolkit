be7ea5e9fc451f2facdf0f73d25c2b2a61f0f748
Nick Barlow <nicholasbarlow1@gmail.com>
2026-04-07 16:40:08 +0100

    chore: Remove line space

--- Modules/CluedIn.Product.Toolkit/private/Invoke-CluedInWebRequest.ps1
+++ Modules/CluedIn.Product.Toolkit/private/Invoke-CluedInWebRequest.ps1
@@ -58,7 +58,6 @@ function Invoke-CluedInWebRequest {
 
             Write-Verbose "Attempting again"
             $requestResult = invokeRequest
-
             if ($requestResult.StatusDescription -eq 'OK') { return ($requestResult.Content | ConvertFrom-Json) }
             else { Write-Error "Refreshed token, but received response: $($requestResult.StatusDescription)"; return }
         }
